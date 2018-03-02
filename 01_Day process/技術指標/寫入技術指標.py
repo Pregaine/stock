9174acea0959c45b127146a42c19cae0c5de477c
wenwei.chung <wenwei.chung@gmail.com>
2018-03-02 23:13:50 +0800

    SQL Cmd 不執行 'SET LANGUAGE us_english;'，就可以寫入技術指標資料庫

--- 01_Day process/技術指標/寫入技術指標.py
+++ 01_Day process/技術指標/寫入技術指標.py
@@ -22,7 +22,7 @@ class DB_TechAnalysis:
 
     def __init__( self, server, database, username, password ):
 
-        cmd = 'SET LANGUAGE us_english;'
+        # cmd = 'SET LANGUAGE us_english;'
 
         self.df = pd.DataFrame( )
         self.src_df = pd.DataFrame( )
@@ -44,7 +44,7 @@ class DB_TechAnalysis:
         self.cur_db = self.con_db.cursor( )
         self.con_db.commit( )
 
-        self.cur_db.execute(cmd)
+        # self.cur_db.execute(cmd)
 
         self.stock = ''
         self.date = ''
